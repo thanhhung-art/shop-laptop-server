60bd36a94bb2d7363524e2adbed4b2ff32425635
thanh-hung <thanhhungdeveloper@gmail.com>
2022-06-07 15:14:42 +0700

    fix eror deploy

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use("/api/review", reviewRouter);
 app.use("/api/stripe/", postCharge);
 
 app.listen((port = process.env.PORT || 5000), () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running`);
 });
 
 module.exports = app;
